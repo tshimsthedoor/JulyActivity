8e70298d69ed2ec8148c68098649396461f12c95
Tresor Tshimanga Kazadi <tshims79@gmail.com>
2021-07-05 11:24:51 +0200

    Add project files.

---
+++ Controllers/AppointmentController1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JulyActivity.Controllers
+{
+    public class AppointmentController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            string todaysDate = DateTime.Now.ToShortDateString();
+            return Ok(todaysDate);
+        }
+    }
+}
