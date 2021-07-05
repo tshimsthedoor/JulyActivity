14522127c977685b42186ac335affedb50b5761f
Tresor Tshimanga Kazadi <tshims79@gmail.com>
2021-07-05 17:20:13 +0200

    Add the connectionString
    - We added the connectionstring as the default

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JulyActivity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JulyActivity.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using JulyActivity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JulyActivity.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+    }
+}
