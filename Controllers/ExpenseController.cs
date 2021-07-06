31bb06afa2cf6c9169ea2a67a191461d8120012f
Tresor Tshimanga Kazadi <tshims79@gmail.com>
2021-07-06 21:48:25 +0200

    Server Side Validation
    - I added the server side validation on the expense

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -35,9 +35,15 @@ namespace JulyActivity.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Expense obj)
         {
-            _db.Expenses.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Expenses.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
         }
     }
 
--- Models/Expense.cs
+++ Models/Expense.cs
@@ -13,8 +13,11 @@ namespace JulyActivity.Models
         public int Id { get; set; }
 
         [DisplayName("Expense")]
+        [Required]
         public string ExpenseName { get; set; }
 
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage ="Amount must be greater than 0!")]
         public int Amount { get; set; }
     }
 }
