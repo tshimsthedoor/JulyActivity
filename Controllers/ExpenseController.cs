3dc7f30f131321d41c1d59a0114f07b051aac08e
Tresor Tshimanga Kazadi <tshims79@gmail.com>
2021-07-07 00:41:27 +0200

    Could Delete
    - I could delete the entry from expense

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -44,19 +44,38 @@ namespace JulyActivity.Controllers
             }
             return View(obj);
 
-        } // Post Delete
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+        }
+        
+        // GET Delete
+                
         public IActionResult Delete(int? id)
         {
-            if (ModelState.IsValid)
+            
+            if(id == null || id == 0)
             {
-                _db.Expenses.Remove(id);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-
+                return NotFound();
             }
-            return View(id);
+            var obj = _db.Expenses.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        } 
+        
+        // Post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Expenses.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Expenses.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
 
         }
     }
